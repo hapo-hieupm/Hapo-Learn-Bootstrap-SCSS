a36139931f3c703b57f6b5362c0b55c741b22800
hapo-hieupm <hieu.pm@haposoft.com>
2020-08-11 11:39:27 +0700

    mess

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('.mess-btn').click(function() {
+        $('.mess-box').toggle();
   });
-  
+
+  $('.btn-close').click(function() {
+        $('.mess-box').hide();
+  });
+
+  $('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
