73e7e3c8df7293c76724b8decbd8f316c7c31e16
hapo-hieupm <hieu.pm@haposoft.com>
2020-08-12 16:06:14 +0700

    whyhapo feedback

--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('.mess-btn').click(function() {
+        $('.mess-box').toggle();
   });
-  
+
+  $('.btn-close').click(function() {
+        $('.mess-box').hide();
+  });
+
+  $('.feedback-slick').slick({
+    centerMode: false,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 500000,
+    prevArrow: $('.left'),
+    nextArrow: $('.right'),
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    
+    responsive: [
+      {
+        
+        breakpoint: 769,
+        settings: {
+     
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+});
